5b40756d78128fba1a9f79ecfb4e6eeca8b3eaf0
Christian Krause <kizkizzbangbang@googlemail.com>
2012-11-26 18:49:56 +0100

    replaces factory defs with factory objects
    - replaces factory defs with factory objects
      - uses AreaChart.apply (no syntax change) for basic chart types
      - uses AreaChart.stacked (syntax change) for specific chart types
    - also drops urls factory parameters
    - also drops labels factory parameters (simplifies a lot of code, since
      now there are the accessors / mutators)

--- src/main/scala/ChartFactory.scala
+++ src/main/scala/ChartFactory.scala
@@ -31,9 +31,7 @@ import org.jfree.data.xy._
 
 import org.jfree.chart.{ JFreeChart, ChartFactory ⇒ JChartFactory }
 import org.jfree.chart.axis._
-import org.jfree.chart.labels._
 import org.jfree.chart.plot._
-import org.jfree.chart.plot.PlotOrientation._
 
 import org.jfree.util.TableOrder
 
@@ -65,268 +63,238 @@ object ChartFactory extends ChartFactory
   * @define orientation the orientation of the chart
   * @define legend      whether or not the chart will contain a legend
   * @define tooltips    whether or not tooltips will be generated
-  * @define urls        whether or not URLs will be generated
-  * @define labels      whether or not the chart will contain labels
   */
 trait ChartFactory {
 
-  /** Creates a new chart that represents numeric `x` and `y` values with an area.
-    *
-    * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
-    * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
-    *
-    * @param dataset     $dataset
-    * @param title       $title
-    * @param xAxisLabel  $xAxisLabel
-    * @param yAxisLabel  $yAxisLabel
-    * @param orientation $orientation
-    * @param legend      $legend
-    * @param tooltips    $tooltips
-    * @param urls        $urls
-    */
-  def AreaChart(dataset: XYDataset,
-                title: String = "",
-                xAxisLabel: String = "",
-                yAxisLabel: String = "",
-                orientation: PlotOrientation = VERTICAL,
-                legend: Boolean = true,
-                tooltips: Boolean = false,
-                urls: Boolean = false): JFreeChart = {
-    val chart = JChartFactory.createXYAreaChart(title, xAxisLabel, yAxisLabel, dataset, orientation,
-      legend, tooltips, urls)
+  /** Factory methods for area charts. */
+  object AreaChart {
 
-    dataset match {
-      case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _ ⇒
-    }
+    /** Creates a new chart that represents numeric `x` and `y` values with an area.
+      *
+      * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
+      * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
+      *
+      * @param dataset     $dataset
+      * @param title       $title
+      * @param xAxisLabel  $xAxisLabel
+      * @param yAxisLabel  $yAxisLabel
+      * @param orientation $orientation
+      * @param legend      $legend
+      * @param tooltips    $tooltips
+      */
+    def apply(dataset: XYDataset,
+              title: String = "",
+              xAxisLabel: String = "",
+              yAxisLabel: String = "",
+              orientation: PlotOrientation = PlotOrientation.VERTICAL,
+              legend: Boolean = true,
+              tooltips: Boolean = false): JFreeChart = {
 
-    chart
-  }
+      val chart = JChartFactory.createXYAreaChart(title, xAxisLabel, yAxisLabel, dataset,
+        orientation, legend, tooltips, false)
 
-  /** Creates a new chart that represents categorized numeric data with bars.
-    *
-    * @param dataset     $dataset
-    * @param title       $title
-    * @param xAxisLabel  $xAxisLabel
-    * @param yAxisLabel  $yAxisLabel
-    * @param orientation $orientation
-    * @param legend      $legend
-    * @param tooltips    $tooltips
-    * @param urls        $urls
-    * @param labels      $labels
-    */
-  def BarChart(dataset: CategoryDataset,
-               title: String = "",
-               xAxisLabel: String = "",
-               yAxisLabel: String = "",
-               orientation: PlotOrientation = VERTICAL,
-               legend: Boolean = true,
-               tooltips: Boolean = false,
-               urls: Boolean = false,
-               labels: Boolean = false): JFreeChart = {
-    val chart = JChartFactory.createBarChart(title, xAxisLabel, yAxisLabel, dataset, orientation,
-      legend, tooltips, urls)
-
-    chart.getPlot match {
-      case plot: CategoryPlot if labels ⇒
-        val renderer = plot.getRenderer
-        renderer.setBaseItemLabelsVisible(true)
-        renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator)
-
-      case _ ⇒
-    }
-
-    chart
-  }
-
-  /** Creates a new chart that represents numeric `x` and `y` values with a line.
-    *
-    * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
-    * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
-    *
-    * @param dataset     $dataset
-    * @param title       $title
-    * @param xAxisLabel  $xAxisLabel
-    * @param yAxisLabel  $yAxisLabel
-    * @param orientation $orientation
-    * @param legend      $legend
-    * @param tooltips    $tooltips
-    * @param urls        $urls
-    */
-  def LineChart(dataset: XYDataset,
-                title: String = "",
-                xAxisLabel: String = "",
-                yAxisLabel: String = "",
-                orientation: PlotOrientation = VERTICAL,
-                legend: Boolean = true,
-                tooltips: Boolean = false,
-                urls: Boolean = false): JFreeChart = {
-    val chart = JChartFactory.createXYLineChart(title, xAxisLabel, yAxisLabel, dataset, orientation,
-      legend, tooltips, urls)
-
-    dataset match {
-      case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _ ⇒
-    }
-
-    chart
-  }
-
-  /** Creates a new chart that represents categorized numeric data with a multiple pie.
-    *
-    * @param dataset  $dataset
-    * @param title    $title
-    * @param legend   $legend
-    * @param tooltips $tooltips
-    * @param urls     $urls
-    * @param labels   $labels
-    */
-  def MultiplePieChart(dataset: CategoryDataset,
-                       title: String = "",
-                       legend: Boolean = true,
-                       tooltips: Boolean = true,
-                       urls: Boolean = false,
-                       labels: Boolean = true): JFreeChart = {
-    val chart = JChartFactory.createMultiplePieChart(title, dataset, TableOrder.BY_COLUMN, legend,
-      tooltips, urls)
-
-    chart.getPlot match {
-      case plot: MultiplePiePlot ⇒ plot.getPieChart.getPlot match {
-        case plot: PiePlot if ! labels ⇒ plot.setLabelGenerator(null)
+      dataset match {
+        case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
         case _ ⇒
       }
-      case _ ⇒
+
+      chart
     }
 
-    chart
-  }
-
-  /** Creates a new chart that represents categorized numeric data with a pie.
-    *
-    * @param dataset          $dataset
-    * @param title            $title
-    * @param legend           $legend
-    * @param tooltips         $tooltips
-    * @param urls             $urls
-    * @param labels           $labels
-    * @param threeDimensional whether or not to generate a 3D pie
-    */
-  def PieChart(dataset: PieDataset,
-               title: String = "",
-               legend: Boolean = true,
-               tooltips: Boolean = true,
-               urls: Boolean = false,
-               labels: Boolean = true,
-               threeDimensional: Boolean = false): JFreeChart = {
-    val chart = if (threeDimensional)
-      JChartFactory.createPieChart3D(title, dataset, legend, tooltips, urls)
-    else
-      JChartFactory.createPieChart(title, dataset, legend, tooltips, urls)
-
-    chart.getPlot match {
-      case plot: PiePlot if ! labels ⇒ plot.setLabelGenerator(null)
-      case _ ⇒
-    }
-
-    chart
-  }
-
-  /** Creates a new chart that represents categorized numeric data in a ring.
-    *
-    * @param dataset          $dataset
-    * @param title            $title
-    * @param legend           $legend
-    * @param tooltips         $tooltips
-    * @param urls             $urls
-    * @param labels           $labels
-    * @param threeDimensional whether or not to generate a 3D pie
-    */
-  def RingChart(dataset: PieDataset,
+    /** Creates a new chart that represents multiple numeric `x` and `y` values with stacked areas.
+      *
+      * If the input dataset is an instance of a `TimeTableXYDataset` the domain axis will correctly
+      * be set to a `DateAxis`.
+      *
+      * @param dataset     $dataset
+      * @param title       $title
+      * @param xAxisLabel  $xAxisLabel
+      * @param yAxisLabel  $yAxisLabel
+      * @param orientation $orientation
+      * @param legend      $legend
+      * @param tooltips    $tooltips
+      */
+    def stacked(dataset: TableXYDataset,
                 title: String = "",
+                xAxisLabel: String = "",
+                yAxisLabel: String = "",
+                orientation: PlotOrientation = PlotOrientation.VERTICAL,
                 legend: Boolean = true,
-                tooltips: Boolean = true,
-                urls: Boolean = false,
-                labels: Boolean = true): JFreeChart = {
-    val chart = JChartFactory.createRingChart(title, dataset, legend, tooltips, urls)
+                tooltips: Boolean = false): JFreeChart = {
 
-    chart.getPlot match {
-      case plot: PiePlot if ! labels ⇒ plot.setLabelGenerator(null)
-      case _ ⇒
+      val chart = JChartFactory.createStackedXYAreaChart(title, xAxisLabel, yAxisLabel, dataset,
+        orientation, legend, tooltips, false)
+
+      dataset match {
+        case _: TimeTableXYDataset ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _ ⇒
+      }
+
+      chart
     }
 
-    chart
+    /** Creates a new chart that represents multiple numeric `x` and `y` values with a stepped area.
+      *
+      * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
+      * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
+      *
+      * @param dataset     $dataset
+      * @param title       $title
+      * @param xAxisLabel  $xAxisLabel
+      * @param yAxisLabel  $yAxisLabel
+      * @param orientation $orientation
+      * @param legend      $legend
+      * @param tooltips    $tooltips
+      */
+    def stepped(dataset: XYDataset,
+                title: String = "",
+                xAxisLabel: String = "",
+                yAxisLabel: String = "",
+                orientation: PlotOrientation = PlotOrientation.VERTICAL,
+                legend: Boolean = true,
+                tooltips: Boolean = false): JFreeChart = {
+
+      val chart = JChartFactory.createXYStepAreaChart(title, xAxisLabel, yAxisLabel, dataset,
+        orientation, legend, tooltips, false)
+
+      dataset match {
+        case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _ ⇒
+      }
+
+      chart
+    }
+
   }
 
-  /** Creates a new chart that represents multiple numeric `x` and `y` values with stacked areas.
-    *
-    * If the input dataset is an instance of a `TimeTableXYDataset` the domain axis will correctly
-    * be set to a `DateAxis`.
-    *
-    * @param dataset     $dataset
-    * @param title       $title
-    * @param xAxisLabel  $xAxisLabel
-    * @param yAxisLabel  $yAxisLabel
-    * @param orientation $orientation
-    * @param legend      $legend
-    * @param tooltips    $tooltips
-    * @param urls        $urls
-    */
-  def StackedAreaChart(dataset: TableXYDataset,
-                       title: String = "",
-                       xAxisLabel: String = "",
-                       yAxisLabel: String = "",
-                       orientation: PlotOrientation = VERTICAL,
-                       legend: Boolean = true,
-                       tooltips: Boolean = false,
-                       urls: Boolean = false): JFreeChart = {
-    val chart = JChartFactory.createStackedXYAreaChart(title, xAxisLabel, yAxisLabel, dataset,
-      orientation, legend, tooltips, urls)
+  /** Factory methods for bar charts. */
+  object BarChart {
 
-    dataset match {
-      case _: TimeTableXYDataset ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _ ⇒
-    }
+    /** Creates a new chart that represents categorized numeric data with bars.
+      *
+      * @param dataset     $dataset
+      * @param title       $title
+      * @param xAxisLabel  $xAxisLabel
+      * @param yAxisLabel  $yAxisLabel
+      * @param orientation $orientation
+      * @param legend      $legend
+      * @param tooltips    $tooltips
+      */
+    def apply(dataset: CategoryDataset,
+              title: String = "",
+              xAxisLabel: String = "",
+              yAxisLabel: String = "",
+              orientation: PlotOrientation = PlotOrientation.VERTICAL,
+              legend: Boolean = true,
+              tooltips: Boolean = false): JFreeChart =
+      JChartFactory.createBarChart(title, xAxisLabel, yAxisLabel, dataset, orientation, legend,
+        tooltips, false)
 
-    chart
   }
 
-  /** Creates a new chart that represents multiple numeric `x` and `y` values with a stepped area.
-    *
-    * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
-    * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
-    *
-    * @param dataset     $dataset
-    * @param title       $title
-    * @param xAxisLabel  $xAxisLabel
-    * @param yAxisLabel  $yAxisLabel
-    * @param orientation $orientation
-    * @param legend      $legend
-    * @param tooltips    $tooltips
-    * @param urls        $urls
-    */
-  def StepAreaChart(dataset: XYDataset,
-                    title: String = "",
-                    xAxisLabel: String = "",
-                    yAxisLabel: String = "",
-                    orientation: PlotOrientation = VERTICAL,
-                    legend: Boolean = true,
-                    tooltips: Boolean = false,
-                    urls: Boolean = false): JFreeChart = {
-    val chart = JChartFactory.createXYStepAreaChart(title, xAxisLabel, yAxisLabel, dataset,
-      orientation, legend, tooltips, urls)
+  /** Factory methods for line charts. */
+  object LineChart {
 
-    dataset match {
-      case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
-      case _ ⇒
+    /** Creates a new chart that represents numeric `x` and `y` values with a line.
+      *
+      * If the input dataset is an instance of a `TimePeriodValuesCollection`, `TimeSeriesCollection`
+      * or `TimeTableXYDataset` the domain axis will correctly be set to a `DateAxis`.
+      *
+      * @param dataset     $dataset
+      * @param title       $title
+      * @param xAxisLabel  $xAxisLabel
+      * @param yAxisLabel  $yAxisLabel
+      * @param orientation $orientation
+      * @param legend      $legend
+      * @param tooltips    $tooltips
+      */
+    def apply(dataset: XYDataset,
+              title: String = "",
+              xAxisLabel: String = "",
+              yAxisLabel: String = "",
+              orientation: PlotOrientation = PlotOrientation.VERTICAL,
+              legend: Boolean = true,
+              tooltips: Boolean = false): JFreeChart = {
+
+      val chart = JChartFactory.createXYLineChart(title, xAxisLabel, yAxisLabel, dataset,
+        orientation, legend, tooltips, false)
+
+      dataset match {
+        case _: TimePeriodValuesCollection ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeSeriesCollection       ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _: TimeTableXYDataset         ⇒ chart.getXYPlot.setDomainAxis(new DateAxis)
+        case _ ⇒
+      }
+
+      chart
     }
 
-    chart
+  }
+
+  /** Factory methods for pie charts. */
+  object PieChart {
+
+    /** Creates a new chart that represents categorized numeric data with a pie.
+      *
+      * @param dataset  $dataset
+      * @param title    $title
+      * @param legend   $legend
+      * @param tooltips $tooltips
+      */
+    def apply(dataset: PieDataset,
+              title: String = "",
+              legend: Boolean = true,
+              tooltips: Boolean = true): JFreeChart =
+      JChartFactory.createPieChart(title, dataset, legend, tooltips, false)
+
+    /** Creates a new chart that represents categorized numeric data with a multiple pie.
+      *
+      * @param dataset  $dataset
+      * @param title    $title
+      * @param legend   $legend
+      * @param tooltips $tooltips
+      */
+    def multiple(dataset: CategoryDataset,
+                 title: String = "",
+                 legend: Boolean = true,
+                 tooltips: Boolean = true): JFreeChart =
+      JChartFactory.createMultiplePieChart(title, dataset, TableOrder.BY_COLUMN, legend, tooltips, false)
+
+    /** Creates a new chart that represents categorized numeric data with a three dimensional pie.
+      *
+      * @param dataset  $dataset
+      * @param title    $title
+      * @param legend   $legend
+      * @param tooltips $tooltips
+      */
+    def threeDimensional(dataset: PieDataset,
+                         title: String = "",
+                         legend: Boolean = true,
+                         tooltips: Boolean = true): JFreeChart =
+      JChartFactory.createPieChart3D(title, dataset, legend, tooltips, false)
+
+  }
+
+  /** Factory methods for ring charts. */
+  object RingChart {
+
+    /** Creates a new chart that represents categorized numeric data in a ring.
+      *
+      * @param dataset  $dataset
+      * @param title    $title
+      * @param legend   $legend
+      * @param tooltips $tooltips
+      */
+    def apply(dataset: PieDataset,
+              title: String = "",
+              legend: Boolean = true,
+              tooltips: Boolean = true): JFreeChart =
+      JChartFactory.createRingChart(title, dataset, legend, tooltips, false)
+
   }
 
 }
