f8131bd33060a65e533dabf99f0d97fb76563a94
Christian Krause <kizkizzbangbang@googlemail.com>
2012-12-26 20:39:33 +0100

    introduces CategoryChart

---
+++ src/main/scala/CategoryChart.scala
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  Copyright © 2012 Christian Krause                                                            *
+ *                                                                                               *
+ *  Christian Krause <kizkizzbangbang@googlemail.com>                                            *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  This file is part of 'sfreechart'.                                                           *
+ *                                                                                               *
+ *  This project is free software: you can redistribute it and/or modify it under the terms      *
+ *  of the GNU Lesser General Public License as published by the Free Software Foundation,       *
+ *  either version 3 of the License, or any later version.                                       *
+ *                                                                                               *
+ *  This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;    *
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    *
+ *  See the GNU Lesser General Public License for more details.                                  *
+ *                                                                                               *
+ *  You should have received a copy of the GNU Lesser General Public License along with this     *
+ *  project. If not, see <http://www.gnu.org/licenses/>.                                         *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+package org.sfree.chart
+
+import org.jfree.chart.plot.CategoryPlot
+
+/** Represents categorized numeric data. */
+trait CategoryChart extends Chart[CategoryPlot] {
+  override def plot: CategoryPlot = peer.getCategoryPlot
+}
