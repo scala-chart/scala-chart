e22d9e39d86895edf1319d1ec608210c2e7d3d88
Christian Krause <kizkizzbangbang@googlemail.com>
2013-01-30 16:11:31 +0100

    adds stacked xy bar chart factory method

--- src/main/scala/XYBarChart.scala
+++ src/main/scala/XYBarChart.scala
@@ -26,10 +26,14 @@ package org.sfree.chart
 
 import scala.swing.Orientation
 
-import org.jfree.chart.ChartFactory.createXYBarChart
-import org.jfree.chart.axis.DateAxis
+import org.jfree.chart.JFreeChart
+import org.jfree.chart.ChartFactory.{ createXYBarChart, getChartTheme }
+import org.jfree.chart.axis.{ DateAxis, NumberAxis }
+import org.jfree.chart.labels.StandardXYToolTipGenerator
+import org.jfree.chart.renderer.xy.StackedXYBarRenderer
+import org.jfree.chart.plot.XYPlot
 import org.jfree.data.time._
-import org.jfree.data.xy.IntervalXYDataset
+import org.jfree.data.xy._
 
 /** Factory for numeric bar charts. */
 object XYBarChart extends ChartFactory {
@@ -65,6 +69,64 @@ object XYBarChart extends ChartFactory {
 
     val chart = createXYBarChart(title, domainAxisLabel, dateAxis, rangeAxisLabel, dataset,
       orientation, legend, tooltips, false)
+
+    new XYChart {
+      override val peer = chart
+    }
+  }
+
+  /** Creates a new chart that represents numeric `x` (intervals) and `y` values with a line.
+    *
+    * If the input dataset is an instance of a `TimePeriodValuesCollection`,
+    * `TimeSeriesCollection` or `TimeTableXYDataset` the domain axis will correctly be set to a
+    * `DateAxis`.
+    *
+    * @param dataset         $dataset
+    * @param title           $title
+    * @param domainAxisLabel $domainAxisLabel
+    * @param rangeAxisLabel  $rangeAxisLabel
+    * @param orientation     $orientation
+    * @param legend          $legend
+    * @param tooltips        $tooltips
+    */
+  def stacked(dataset: IntervalXYDataset with TableXYDataset,
+              title: String = "",
+              domainAxisLabel: String = "",
+              rangeAxisLabel: String = "",
+              orientation: Orientation.Value = Orientation.Vertical,
+              legend: Boolean = true,
+              tooltips: Boolean = false): XYChart = {
+
+    val dateAxis = dataset match {
+      case _: TimeTableXYDataset ⇒ true
+      case _                     ⇒ false
+    }
+
+    val domainAxis = if (dateAxis) {
+      new DateAxis(domainAxisLabel)
+    } else {
+      val axis = new NumberAxis(domainAxisLabel)
+      axis.setAutoRangeIncludesZero(false)
+      axis
+    }
+
+    val valueAxis = new NumberAxis(rangeAxisLabel)
+
+    val renderer = new StackedXYBarRenderer()
+
+    if (tooltips) {
+      if (dateAxis) {
+        renderer.setBaseToolTipGenerator(StandardXYToolTipGenerator.getTimeSeriesInstance)
+      } else {
+        renderer.setBaseToolTipGenerator(new StandardXYToolTipGenerator())
+      }
+    }
+
+    val plot = new XYPlot(dataset, domainAxis, valueAxis, renderer)
+    plot.setOrientation(orientation)
+
+    val chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, legend)
+    getChartTheme()(chart)
 
     new XYChart {
       override val peer = chart
