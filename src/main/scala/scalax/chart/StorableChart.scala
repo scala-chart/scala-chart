673762cdb64894ebecc9210fe5468c3eaa322710
Christian Krause <kizkizzbangbang@googlemail.com>
2013-06-07 06:51:19 +0200

    using our own writers for saving charts, using String rather than File for output file name

--- src/main/scala/scalax/chart/StorableChart.scala
+++ src/main/scala/scalax/chart/StorableChart.scala
@@ -24,54 +24,46 @@
 
 package scalax.chart
 
-import java.io._
-
-import org.jfree.chart.ChartUtilities
+import java.io.FileOutputStream
 
 import com.lowagie.text.pdf.{ DefaultFontMapper, FontMapper }
 
 /** Provides methods for saving a chart.
   *
-  * @define output the output file
+  * @define file the output file
   */
 trait StorableChart extends WritableChart {
 
   self: Chart[_] ⇒
 
+  private def managed[R <: FileOutputStream](r: R)(f: R ⇒ Unit): Unit = try { f(r) } finally { r.close() }
+
   /** Saves the chart as a JPEG image.
     *
-    * @param output $output
-    * @param dim    $dim
+    * @param file $file
+    * @param dim  $dim
     */
-  def saveAsJPEG(output: File, dim: (Int,Int)) {
-    val (width,height) = dim
-    ChartUtilities.saveChartAsJPEG(output, peer, width, height)
+  def saveAsJPEG(file: String, dim: (Int,Int)) {
+    managed(new FileOutputStream(file)) { os ⇒ writeAsJPEG(os, dim) }
   }
 
   /** Saves the chart as a PDF.
     *
-    * @param output     $output
+    * @param file       $file
     * @param dim        $dim
     * @param fontMapper $fontMapper
     */
-  def saveAsPDF(output: File, dim: (Int,Int), fontMapper: FontMapper = new DefaultFontMapper) {
-    val os = new BufferedOutputStream(new FileOutputStream(output))
-
-    try {
-      writeAsPDF(os, dim, fontMapper)
-    } finally {
-      os.close()
-    }
+  def saveAsPDF(file: String, dim: (Int,Int), fontMapper: FontMapper = new DefaultFontMapper) {
+    managed(new FileOutputStream(file)) { os ⇒ writeAsPDF(os, dim, fontMapper) }
   }
 
   /** Saves the chart as a PNG image.
     *
-    * @param output $output
-    * @param dim    $dim
+    * @param file $file
+    * @param dim  $dim
     */
-  def saveAsPNG(output: File, dim: (Int,Int)) {
-    val (width,height) = dim
-    ChartUtilities.saveChartAsPNG(output, peer, width, height)
+  def saveAsPNG(file: String, dim: (Int,Int)) {
+    managed(new FileOutputStream(file)) { os ⇒ writeAsPNG(os, dim) }
   }
 
 }
