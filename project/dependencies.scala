47976f52f94b9e880a6af5be2d79bb54db9102a2
Christian Krause <kizkizzbangbang@googlemail.com>
2013-08-27 13:07:35 +0200

    sbt and jfreechart version bumps

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ package build
 import sbt._
 
 object Dependencies {
-  val jfreechart = "org.jfree"      %  "jfreechart"  % "1.0.14"
+  val jfreechart = "org.jfree"      %  "jfreechart"  % "1.0.15"
   val specs2     = "org.specs2"     %% "specs2"      % "2.1.1"
   val swing      = "org.scala-lang" %  "scala-swing"
 }
