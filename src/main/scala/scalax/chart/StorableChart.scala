a0121ca4eff98a655313f68657c6fbaedd338b46
Christian Krause <kizkizzbangbang@googlemail.com>
2014-01-28 22:14:23 +0100

    removes license headers

--- src/main/scala/scalax/chart/StorableChart.scala
+++ src/main/scala/scalax/chart/StorableChart.scala
@@ -1,27 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                                               *
- *  Copyright © 2012-2013 Christian Krause                                                       *
- *                                                                                               *
- *  Christian Krause <kizkizzbangbang@googlemail.com>                                            *
- *                                                                                               *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                                               *
- *  This file is part of 'scala-chart'.                                                          *
- *                                                                                               *
- *  This project is free software: you can redistribute it and/or modify it under the terms      *
- *  of the GNU Lesser General Public License as published by the Free Software Foundation,       *
- *  either version 3 of the License, or any later version.                                       *
- *                                                                                               *
- *  This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;    *
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    *
- *  See the GNU Lesser General Public License for more details.                                  *
- *                                                                                               *
- *  You should have received a copy of the GNU Lesser General Public License along with this     *
- *  project. If not, see <http://www.gnu.org/licenses/>.                                         *
- *                                                                                               *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-
 package scalax.chart
 
 import java.io.FileOutputStream
