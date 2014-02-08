d96b3c4717fe66148c062e0ac32efa074401375d
Christian Krause <kizkizzbangbang@googlemail.com>
2014-02-08 15:30:21 +0100

    better doc macros integration for chart companion

--- src/main/scala/scalax/chart/ChartCompanion.scala
+++ src/main/scala/scalax/chart/ChartCompanion.scala
@@ -4,12 +4,19 @@ import org.jfree.chart.ChartTheme
 import org.jfree.chart.JFreeChart
 
 /** A template class for companion objects of [[Chart]] classes. */
-abstract class ChartCompanion[C <: Chart[_]] protected () {
+abstract class ChartCompanion[C <: Chart[_]] protected () extends DocMacros {
 
-  /** Returns a new chart using an explicit peer. */
+  /** Returns a new $chart using an explicit peer.
+    *
+    * @param jfree $peer
+    */
   def fromPeer(jfree: JFreeChart): C
 
-  /** Returns a new chart using an explicit peer with the theme applied. */
+  /** Returns a new $chart using an explicit peer with the theme applied.
+    *
+    * @param jfree $peer
+    * @param theme $theme
+    */
   final def fromPeer(jfree: JFreeChart, theme: ChartTheme): C = {
     theme.apply(jfree)
     fromPeer(jfree)
--- src/main/scala/scalax/chart/DocMacros.scala
+++ src/main/scala/scalax/chart/DocMacros.scala
@@ -1,24 +1,39 @@
 package scalax.chart
 
-/** Mixin solely for the purpose of providing common documentation macros. The following is a list
-  * of the provided macros and their assigned text:
+/** Mixin solely for the purpose of providing common documentation macros.
   *
-  *  - '''`data`''': $data
-  *  - '''`domainAxisLabel`''': $domainAxisLabel
-  *  - '''`legend`''': $legend
-  *  - '''`orientation`''': $orientation
-  *  - '''`rangeAxisLabel`''': $rangeAxisLabel
-  *  - '''`theme`''': $theme
-  *  - '''`title`''': $title
-  *  - '''`tooltips`''': $tooltips
+  * == Concrete ==
   *
-  * @define data the data the chart will visualize
+  * The following list contains macros that are usually overridden by concrete representations:
+  *
+  *  - '''`\$chart`''' = $chart
+  *  - '''`\$Chart`''' = $Chart
+  *
+  * == Basic ==
+  *
+  * The following list contains basic macros that are usually not overridden:
+  *
+  *  - '''`\$data`''' = $data
+  *  - '''`\$domainAxisLabel`''' = $domainAxisLabel
+  *  - '''`\$legend`''' = $legend
+  *  - '''`\$orientation`''' = $orientation
+  *  - '''`\$peer`''' = $peer
+  *  - '''`\$rangeAxisLabel`''' = $rangeAxisLabel
+  *  - '''`\$theme`''' = $theme
+  *  - '''`\$title`''' = $title
+  *  - '''`\$tooltips`''' = $tooltips
+  *
+  * @define chart chart
+  * @define Chart Chart
+  *
+  * @define data the data the $chart will visualize
   * @define domainAxisLabel the label for the domain axis
-  * @define legend whether or not the chart will contain a legend
-  * @define orientation the orientation of the chart
+  * @define legend whether or not the $chart will contain a legend
+  * @define orientation the orientation of the $chart
+  * @define peer the underlying $chart
   * @define rangeAxisLabel the label for the range axis
-  * @define theme the theme to apply to the chart
-  * @define title the title of the chart
+  * @define theme the theme to apply to the $chart
+  * @define title the title of the $chart
   * @define tooltips whether or not tooltips will be generated
   */
 trait DocMacros
