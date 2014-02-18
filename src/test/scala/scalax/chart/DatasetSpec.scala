9cc2b6ef20fac1a647d8de8fca9f86c5fa585d9f
Christian Krause <kizkizzbangbang@googlemail.com>
2014-02-18 16:08:12 +0100

    all tests related to the series's conversions

--- src/test/scala/scalax/chart/DatasetSpec.scala
+++ src/test/scala/scalax/chart/DatasetSpec.scala
@@ -35,11 +35,9 @@ class DatasetSpec extends Specification { def is = s2"""
     implicit conversion
       from Coll[(A,B)]                                                                    $pdi1
 
-  TimePeriodValues
-    explicit conversion
-      from Coll[(A,B)]                                                                    $tpve1
-
   TimePeriodValuesCollection
+    TimePeriodValues
+      from Coll[(A,B)]                                                                    $tpv
     explicit conversion
       from Coll[(A,B)]                                                                    $tpvce1
     implicit conversion
@@ -47,31 +45,33 @@ class DatasetSpec extends Specification { def is = s2"""
       from Coll[TimePeriodValues]                                                         $tpvci2
 
   TimeSeriesCollection
+    TimeSeries
+      from Coll[(A,B)]                                                                    $ts
     implicit conversion
-      from Coll[TimeSeries]                                                               $tsci1
+      from TimeSeries                                                                     $tsci1
+      from Coll[TimeSeries]                                                               $tsci2
 
   XYSeriesCollection
+    XYSeries
+      from Coll[(A,B)]                                                                    $xys
     explicit conversion
       from Coll[(A,B)]                                                                    $xysce1
       from Coll[(A,Coll[(B,C)])]                                                          $xysce2
     implicit conversion
-      from Coll[XYSeries]                                                                 $xysci1
-      from Coll[(A,B)]                                                                    $xysci2
-      from Coll[(A,Coll[(B,C)])]                                                          $xysci3
-
-  YIntervalSeries
-    explicit conversion
-      from Coll[(A,B,C,D)]                                                                $yisec1
-      from Coll[(A,(B,C,D))]                                                              $yisec2
+      from XYSeries                                                                       $xysci1
+      from Coll[XYSeries]                                                                 $xysci2
+      from Coll[(A,B)]                                                                    $xysci3
+      from Coll[(A,Coll[(B,C)])]                                                          $xysci4
 
   YIntervalSeriesCollection
+    YIntervalSeries
+      from Coll[(A,B,C,D)]                                                                $yis
     explicit conversion
-      from Coll[(A,B,C,D)]                                                                $yiscec2
-      from Coll[(A,(B,C,D))]                                                              $yiscec3
-      from Coll[(A,Coll[(B,C,D,E)])]                                                      $yiscec4
-      from Coll[(A,Coll[(B,(C,D,E))])]                                                    $yiscec5
+      from Coll[(A,B,C,D)]                                                                $yisce1
+      from Coll[(A,Coll[(B,C,D,E)])]                                                      $yisce2
     implicit conversion
-      from Coll[YIntervalSeries]                                                          $yisci1
+      from YIntervalSeries                                                                $yisci1
+      from Coll[YIntervalSeries]                                                          $yisci2
                                                                                                  """
   // -----------------------------------------------------------------------------------------------
   // tests
@@ -145,10 +145,9 @@ class DatasetSpec extends Specification { def is = s2"""
     hasIPD(data)
   }
 
-  def tpve1 = {
-    val data = List((new Date,1))
-    val dataset = data.toTimePeriodValues()
-    dataset must beAnInstanceOf[TimePeriodValues]
+  def tpv = {
+    val series = List((new Date,1)) toTimePeriodValues "series name"
+    series must beAnInstanceOf[TimePeriodValues]
   }
 
   def tpvce1 = {
@@ -158,18 +157,33 @@ class DatasetSpec extends Specification { def is = s2"""
   }
 
   def tpvci1 = {
-    val data = List((new Date,1)).toTimePeriodValues()
+    val data = List((new Date,1)) toTimePeriodValues "series name"
     hasIXYD(data)
   }
 
   def tpvci2 = {
-    val data = List((new Date,1)).toTimePeriodValues()
+    val data = List((new Date,1)) toTimePeriodValues "series name"
     hasIXYD(List(data,data))
   }
 
+  def ts = {
+    val series = List((new Date,1)) toTimeSeries "series name"
+    series must beAnInstanceOf[TimeSeries]
+  }
+
   def tsci1 = {
-    val data = List((new Date,1)).toTimeSeries()
+    val data = List((new Date,1)) toTimeSeries "series name"
+    hasIXYD(data)
+  }
+
+  def tsci2 = {
+    val data = List((new Date,1)) toTimeSeries "series name"
     hasIXYD(List(data,data))
+  }
+
+  def xys = {
+    val series = List((1,2)) toXYSeries "series name"
+    series must beAnInstanceOf[XYSeries]
   }
 
   def xysce1 = {
@@ -185,63 +199,51 @@ class DatasetSpec extends Specification { def is = s2"""
   }
 
   def xysci1 = {
-    val s1 = List((1,2)).toXYSeries("s1")
-    val s2 = List((3,4)).toXYSeries("s2")
-    val data = List(s1,s2)
+    val data = List((1,2)) toXYSeries "series name"
     hasIXYD(data)
   }
 
   def xysci2 = {
+    val s1 = List((1,2)) toXYSeries "series name 1"
+    val s2 = List((1,2)) toXYSeries "series name 2"
+    hasIXYD(List(s1,s2))
+  }
+
+  def xysci3 = {
     val data = List((1,2))
     hasIXYD(data)
   }
 
-  def xysci3 = {
+  def xysci4 = {
     val data = List("series" -> List((1,2)))
     hasIXYD(data)
   }
 
-  def yisec1 = {
-    val data = List((1,3,2,4))
-    val series = data.toYIntervalSeries()
+  def yis = {
+    val series = List((1,3,2,4)) toYIntervalSeries "series name"
     series must beAnInstanceOf[YIntervalSeries]
   }
 
-  def yisec2 = {
-    val data = List(1 -> (3,2,4))
-    val series = data.toYIntervalSeries()
-    series must beAnInstanceOf[YIntervalSeries]
-  }
-
-  def yiscec2 = {
+  def yisce1 = {
     val data = List((1,3,2,4))
     val dataset = data.toYIntervalSeriesCollection()
     dataset must beAnInstanceOf[YIntervalSeriesCollection]
   }
 
-  def yiscec3 = {
-    val data = List(1 -> (3,2,4))
-    val dataset = data.toYIntervalSeriesCollection()
-    dataset must beAnInstanceOf[YIntervalSeriesCollection]
-  }
-
-  def yiscec4 = {
+  def yisce2 = {
     val data = List("name" -> List((1,3,2,4)))
     val dataset = data.toYIntervalSeriesCollection()
     dataset must beAnInstanceOf[YIntervalSeriesCollection]
   }
 
-  def yiscec5 = {
-    val data = List("name" -> List(1 -> (3,2,4)))
-    val dataset = data.toYIntervalSeriesCollection()
-    dataset must beAnInstanceOf[YIntervalSeriesCollection]
+  def yisci1 = {
+    val data = List((1,2,3,4)) toYIntervalSeries "series name"
+    hasIXYD(data)
   }
 
-  def yisci1 = {
-    val s1 = List(1 -> (3,2,4)).toYIntervalSeries("s1")
-    val s2 = List(1 -> (3,2,4)).toYIntervalSeries("s2")
-    val data = List(s1,s2)
-    hasIXYD(data)
+  def yisci2 = {
+    val data = List((1,2,3,4)) toYIntervalSeries "series name"
+    hasIXYD(List(data,data))
   }
 
   // -----------------------------------------------------------------------------------------------
