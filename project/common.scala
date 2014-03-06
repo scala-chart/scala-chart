7830afe0649179d36412e319729f681d90e3a21b
Christian Krause <kizkizzbangbang@googlemail.com>
2014-03-06 07:18:52 +0100

    update to 2.11.0-RC1, 2.10 in cross

--- project/common.scala
+++ project/common.scala
@@ -7,12 +7,8 @@ package object build {
   val commonSettings = Seq (
     organization := "com.github.wookietreiber",
     version      := "0.4.0-SNAPSHOT",
-    scalaVersion := "2.10.3",
-    crossScalaVersions := Seq("2.10.4-RC3", "2.11.0-M8"),
-    initialCommands in (Compile, consoleQuick) <<= initialCommands in Compile,
-    initialCommands in Compile in console += """
-      import scalax.chart.api._
-    """
+    scalaVersion := "2.11.0-RC1",
+    crossScalaVersions := Seq("2.11.0-RC1", "2.10.3")
   )
 
   val docURL = "https://github.com/wookietreiber/scala-chart/tree/develop€{FILE_PATH}.scala"
--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,12 +9,12 @@ object Dependencies {
 
   def specs(scalaVersion: String) = scalaVersion match {
     case v if v startsWith "2.10" => Seq("org.specs2"  %% "specs2-core" % "2.3.8" % "test")
-    case "2.11.0-M8"              => Seq("org.specs2"  %% "specs2-core" % "2.3.7" % "test")
+    case v if v startsWith "2.11" => Seq("org.specs2"  %% "specs2-core" % "2.3.10" % "test")
     case _                        => Nil
   }
 
   def swing(scalaVersion: String) = scalaVersion match {
-    case v if v startsWith "2.11" => "org.scala-lang.modules" %% "scala-swing" % "1.0.0-RC2"
+    case v if v startsWith "2.11" => "org.scala-lang.modules" %% "scala-swing" % "1.0.0"
     case v                        => "org.scala-lang"         %  "scala-swing" % v
   }
 }
