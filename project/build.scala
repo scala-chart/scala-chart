153649ba0551cdb813db7ade1bef636439a7de8e
Hanns Holger Rutz <contact@sciss.de>
2014-03-21 15:53:12 +0100

    retains 2.10 release of version 0.3.0, while adding necessary changes to compile and publish with Scala 2.11.0-RC3

--- project/build.scala
+++ project/build.scala
@@ -8,10 +8,11 @@ package object build {
     organization := "com.github.wookietreiber",
     version      := "0.3.0",
     scalaVersion := "2.10.3",
+    crossScalaVersions := Seq("2.11.0-RC3", "2.10.4"),
     initialCommands in Compile in console += """
-      import scalax.chart._
-      import scalax.chart.Charting._
-    """
+      |import scalax.chart._
+      |import scalax.chart.Charting._
+      |""".stripMargin
   )
 
   val docURL = "https://github.com/wookietreiber/scala-chart/tree/develop€{FILE_PATH}.scala"
