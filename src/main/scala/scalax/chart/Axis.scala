14d664909c0175d30d01ddf31480df11bdf43993
Christian Krause <kizkizzbangbang@googlemail.com>
2014-04-13 21:29:32 +0200

    tick marks subclass for axis

--- src/main/scala/scalax/chart/Axis.scala
+++ src/main/scala/scalax/chart/Axis.scala
@@ -9,14 +9,14 @@ import module.Imports._
   */
 abstract class Axis protected () {
 
-  /** Returns the peer axis type. */
+  /** Returns the peer $axis type. */
   type Peer <: org.jfree.chart.axis.Axis
 
   /** Returns the underlying $axis. */
   def peer: Peer
 
   /** Represents the label of an $axis. */
-  class Label private[Axis] () {
+  final class Label private[chart] () {
 
     /** Returns the text of this label. */
     final def text: String =
@@ -72,7 +72,7 @@ abstract class Axis protected () {
   final val label: Label = new Label()
 
   /** Represents the line of an $axis. */
-  class Line private[Axis] () {
+  final class Line private[chart] () {
 
     /** Returns the paint used to draw this line. */
     final def paint: Paint =
@@ -102,6 +102,102 @@ abstract class Axis protected () {
 
   /** Returns the line of this $axis. */
   final val line: Line = new Line()
+
+  /** Represents the tick marks of an $axis. */
+  final class Ticks private[chart] () {
+
+    /** Represents the labels of tick marks of an $axis. */
+    final class Label private[chart] () {
+
+      /** Returns the font of these tick labels. */
+      final def font: Font =
+        peer.getTickLabelFont
+
+      /** Sets the font of these tick labels. */
+      final def font_=(font: Font): Unit =
+        peer.setTickLabelFont(font)
+
+      /** Returns the insets (surrounding blank space) of these tick labels. */
+      final def insets: RectangleInsets =
+        peer.getTickLabelInsets
+
+      /** Sets the insets (surrounding blank space) of these tick labels. */
+      final def insets_=(insets: RectangleInsets): Unit =
+        peer.setTickLabelInsets(insets)
+
+      /** Returns the paint used to draw these tick labels. */
+      final def paint: Paint =
+        peer.getTickLabelPaint
+
+      /** Sets the paint used to draw these tick labels. */
+      final def paint_=(paint: Paint): Unit =
+        peer.setTickLabelPaint(paint)
+
+      /** Returns true if these tick labels are visible. */
+      final def visible: Boolean =
+        peer.isTickLabelsVisible
+
+      /** Sets whether these tick labels are visible. */
+      final def visible_=(b: Boolean): Unit =
+        peer.setTickLabelsVisible(b)
+
+    }
+
+    /** Returns the tick mark labels of this $axis. */
+    final val label: Label = new Label()
+
+    /** Represents the minor ticks of an $axis. */
+    final class Minor private[chart] () {
+
+      /** Returns the inside length of these minor tick marks. */
+      final def insideLength: Float =
+        peer.getMinorTickMarkInsideLength
+
+      /** Sets the inside length of these minor tick marks. */
+      final def insideLength_=(length: Float): Unit =
+        peer.setMinorTickMarkInsideLength(length)
+
+      /** Returns the outside length of these minor tick marks. */
+      final def outsideLength: Float =
+        peer.getMinorTickMarkOutsideLength
+
+      /** Sets the outside length of these minor tick marks. */
+      final def outsideLength_=(length: Float): Unit =
+        peer.setMinorTickMarkOutsideLength(length)
+
+      /** Returns true if these minor tick lines are visible. */
+      final def visible: Boolean =
+        peer.isTickLabelsVisible
+
+      /** Sets whether these minor tick lines are visible. */
+      final def visible_=(b: Boolean): Unit =
+        peer.setTickLabelsVisible(b)
+
+    }
+
+    /** Returns the minor tick marks of this $axis. */
+    final val minor: Minor = new Minor()
+
+    /** Returns the paint used to draw these tick marks. */
+    final def paint: Paint =
+      peer.getTickMarkPaint
+
+    /** Sets the paint used to draw these tick marks. */
+    final def paint_=(paint: Paint): Unit =
+      peer.setTickMarkPaint(paint)
+
+    /** Returns the stroke used to draw these tick marks. */
+    final def stroke: Stroke =
+      peer.getTickMarkStroke
+
+    /** Sets the stroke used to draw these tick marks. */
+    final def stroke_=(stroke: Stroke): Unit =
+      peer.setTickMarkStroke(stroke)
+
+  }
+
+  /** Returns the tick marks of this $axis. */
+  final val ticks: Ticks = new Ticks()
 
   /** Returns true if this $axis is visible. */
   final def visible: Boolean =
