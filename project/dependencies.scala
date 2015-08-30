da34c2977efacdd451949072d7223280a5639e00
Christian Krause <kizkizzbangbang@googlemail.com>
2015-08-30 15:06:34 +0200

    version bumps

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,13 +9,15 @@ object Dependencies {
   val itext      = "com.itextpdf" % "itextpdf"   % "5.5.6"
 
   def Specs2(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
+    case Some((2,12)) => List("org.specs2" %% "specs2-core" % "3.6.4")
     case Some((2,11)) => List("org.specs2" %% "specs2-core" % "3.6.4")
     case Some((2,10)) => List("org.specs2" %% "specs2-core" % "3.6.4")
     case _            => Nil
   }
 
   def Swing(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
-    case Some((2,minor)) if minor >= 11 => "org.scala-lang.modules" %% "scala-swing" % "1.0.2"
+    case Some((2,minor)) if minor >= 12 => "org.scala-lang.modules" %% "scala-swing" % "2.0.0-M2"
+    case Some((2,minor)) if minor == 11 => "org.scala-lang.modules" %% "scala-swing" % "1.0.2"
     case _                              => "org.scala-lang"         %  "scala-swing" % scalaVersion
   }
 }
