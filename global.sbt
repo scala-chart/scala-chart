9f4c6a8898b2deaffae91716fbb51a0529383855
Christian Krause <kizkizzbangbang@googlemail.com>
2016-03-05 12:07:06 +0100

    bump dependencies

--- global.sbt
+++ global.sbt
@@ -2,4 +2,4 @@ organization in ThisBuild := "com.github.wookietreiber"
 
 scalaVersion in ThisBuild := "2.11.7"
 
-crossScalaVersions in ThisBuild := Seq("2.12.0-M2", "2.11.7", "2.10.6")
+crossScalaVersions in ThisBuild := Seq("2.12.0-M3", "2.11.7", "2.10.6")
--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,12 +6,12 @@ import sbt._
 object Dependencies {
   val jfreechart = "org.jfree"    % "jfreechart" % "1.0.19"
   val jfreesvg   = "org.jfree"    % "jfreesvg"   % "3.0"
-  val itext      = "com.itextpdf" % "itextpdf"   % "5.5.6"
+  val itext      = "com.itextpdf" % "itextpdf"   % "5.5.8"
 
   def Specs2(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
-    case Some((2,12)) => List("org.specs2" %% "specs2-core" % "3.6.4")
-    case Some((2,11)) => List("org.specs2" %% "specs2-core" % "3.6.4")
-    case Some((2,10)) => List("org.specs2" %% "specs2-core" % "3.6.4")
+    case Some((2,12)) => List("org.specs2" %% "specs2-core" % "3.7.2")
+    case Some((2,11)) => List("org.specs2" %% "specs2-core" % "3.7.2")
+    case Some((2,10)) => List("org.specs2" %% "specs2-core" % "3.7.2")
     case _            => Nil
   }
 
