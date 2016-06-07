747ec99364fafaa2065dc83bc1d042b1ab17681f
Christian Krause <kizkizzbangbang@googlemail.com>
2016-06-07 11:12:04 +0200

    update dependencies

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,13 +5,13 @@ import sbt._
 
 object Dependencies {
   val jfreechart = "org.jfree"    % "jfreechart" % "1.0.19"
-  val jfreesvg   = "org.jfree"    % "jfreesvg"   % "3.0"
-  val itext      = "com.itextpdf" % "itextpdf"   % "5.5.8"
+  val jfreesvg   = "org.jfree"    % "jfreesvg"   % "3.1"
+  val itext      = "com.itextpdf" % "itextpdf"   % "5.5.9"
 
   def Specs2(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
-    case Some((2,12)) => List("org.specs2" %% "specs2-core" % "3.7.2")
-    case Some((2,11)) => List("org.specs2" %% "specs2-core" % "3.7.2")
-    case Some((2,10)) => List("org.specs2" %% "specs2-core" % "3.7.2")
+    case Some((2,12)) => List("org.specs2" %% "specs2-core" % "3.8.3")
+    case Some((2,11)) => List("org.specs2" %% "specs2-core" % "3.8.3")
+    case Some((2,10)) => List("org.specs2" %% "specs2-core" % "3.8.3")
     case _            => Nil
   }
 
